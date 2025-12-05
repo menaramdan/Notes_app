9badee5cc41e7d4276f5de1fe0bc0bcda661c4fa
Mena <menar5693@gmail.com>
2025-12-05 08:29:12 +0200

    this is the final update

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const NotesAPP());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class NotesAPP extends StatelessWidget {
+  const NotesAPP({super.key});
 
 
 
