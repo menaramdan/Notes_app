be131cfd10b0da707ab4e784a95a1bf60044ccbe
Mena <menar5693@gmail.com>
2025-12-08 12:14:20 +0200

    add hive package on App

---
+++ lib/models/note_model.dart
@@ -0,0 +1,12 @@
+
+class NoteModel {
+  final String title;
+  final String subtitle;
+  final String data; 
+  final int color;
+  NoteModel( {
+    required this.data,
+    required this.title,
+    required this.subtitle,
+    required this.color});
+}
--- lib/widgets/edit_notebody _view.dart
+++ lib/widgets/edit_notebody _view.dart
@@ -1,14 +1,21 @@
 
 import 'package:flutter/material.dart';
+import 'package:notes_app/widgets/custom_icon_%20add_note.dart';
+import 'package:notes_app/widgets/custom_text_field.dart';
 import 'package:notes_app/widgets/notes_view_appbar.dart';
 class Editnotbodysview extends StatelessWidget {
   const Editnotbodysview({super.key});
   @override
   Widget build(BuildContext context) {
-    return Column(
+    return ListView(
       children: [
-        
         NotesViewAppbar(title: 'EditNote', icon: Icons.check),
+        SizedBox(height: 18,),
+        CustomTextField(hint: 'title'),
+        SizedBox(height: 18,),
+        CustomTextField(hint: 'content',maxlines: 5,),
+        SizedBox(height: 25,),
+        CustomIconaddNote()
       ],
     );
   }
