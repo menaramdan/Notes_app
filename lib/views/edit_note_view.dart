56f63763ff81cb4907539868ea6d656f986e43d5
Mena <menar5693@gmail.com>
2025-12-08 04:32:11 +0200

    create editnote view

---
+++ lib/views/edit_note_view.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class EditNoteView extends StatelessWidget {
+  const EditNoteView({super.key});
+  static String id = 'EditNoteView'; 
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
