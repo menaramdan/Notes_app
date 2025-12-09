ac5e1e2b8611983f9a4118d1e4056f9b132e08a9
Mena <menar5693@gmail.com>
2025-12-09 07:09:14 +0200

    we generate code by hive

--- lib/models/note_model.dart
+++ lib/models/note_model.dart
@@ -1,9 +1,17 @@
 
-class NoteModel {
+import 'package:hive_flutter/adapters.dart';
+part 'note_model.g.dart';
+@HiveType(typeId: 0)
+class NoteModel extends HiveObject {
+  @HiveField(0)
   final String title;
+  @HiveField(1)
   final String subtitle;
+  @HiveField(2)
   final String data; 
+  @HiveField(3)
   final int color;
+  
   NoteModel( {
     required this.data,
     required this.title,
