218208e56fbdee06619d2143f8e2c96c0b2040c0
Mena <menar5693@gmail.com>
2025-12-12 12:05:55 +0200

    create blocprovider in view_note

--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:notes_app/cubits/add_note_cubit/read_not_cubit/read_note_cubit.dart';
 import 'package:notes_app/widgets/add_new_note_bottom.dart';
 import 'package:notes_app/widgets/notes_view_body.dart';
 
@@ -6,21 +8,25 @@ class NotesView extends StatelessWidget {
   const NotesView({super.key});
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          showModalBottomSheet(
-            
-            context: context,
-            builder: (context) {
-              return AddNewNote();
-            },
-          );
-        },
-        shape: RoundedRectangleBorder(borderRadius: BorderRadiusGeometry.circular(16)),
-        child: Icon(Icons.add),
+    return BlocProvider(
+      create: (context) => ReadNoteCubit(),
+      child: Scaffold(
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            showModalBottomSheet(
+              context: context,
+              builder: (context) {
+                return AddNewNote();
+              },
+            );
+          },
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadiusGeometry.circular(16),
+          ),
+          child: Icon(Icons.add),
+        ),
+        body: const NotesViewBody(),
       ),
-      body: const NotesViewBody(),
     );
   }
 }
